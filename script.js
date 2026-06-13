13c602ed191b83a7346e14f8c896d123e45b378c
Sonny Putra <psonny322@gmail.com>
2026-06-14 01:32:36 +0700

    upgrade to MyWork Pro system

--- script.js
+++ script.js
@@ -1,1 +1,33 @@
-console.log("MyWork Bantunala System Active 🚀");
+let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
+
+function renderTasks() {
+  const list = document.getElementById("taskList");
+  list.innerHTML = "";
+
+  tasks.forEach((task, index) => {
+    list.innerHTML += `
+      <div class="task">
+        ${task}
+        <button class="delete" onclick="deleteTask(${index})">X</button>
+      </div>
+    `;
+  });
+
+  localStorage.setItem("tasks", JSON.stringify(tasks));
+}
+
+function addTask() {
+  const input = document.getElementById("taskInput");
+  if (input.value.trim() === "") return;
+
+  tasks.push(input.value);
+  input.value = "";
+  renderTasks();
+}
+
+function deleteTask(index) {
+  tasks.splice(index, 1);
+  renderTasks();
+}
+
+renderTasks();
