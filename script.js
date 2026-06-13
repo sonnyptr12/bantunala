473f8a114cb887f8dbd0b09d67a517e59a046a5a
Sonny Putra <psonny322@gmail.com>
2026-06-14 02:07:32 +0700

    upgrade to MyWork Pro system

--- script.js
+++ script.js
@@ -1,33 +1,67 @@
-let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
+const supabaseUrl = "https://scefyffuqtpavzpolrvj.supabase.co";
+const supabaseKey = "sb_publishable_UEEIA0b0Cw3ucS8OoP0ZPQ_9N6iAmGc";
 
-function renderTasks() {
+const supabase = window.supabase.createClient(supabaseUrl, supabaseKey);
+
+// LOGIN
+async function login() {
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+
+  const { data, error } = await supabase.auth.signInWithPassword({
+    email,
+    password
+  });
+
+  if (error) {
+    alert("Login gagal");
+  } else {
+    checkUser();
+  }
+}
+
+// LOGOUT
+async function logout() {
+  await supabase.auth.signOut();
+  checkUser();
+}
+
+// CEK SESSION
+async function checkUser() {
+  const { data } = await supabase.auth.getSession();
+
+  if (data.session) {
+    document.getElementById("authBox").style.display = "none";
+    document.getElementById("app").style.display = "block";
+    loadTasks();
+  } else {
+    document.getElementById("authBox").style.display = "block";
+    document.getElementById("app").style.display = "none";
+  }
+}
+
+// TASK SYSTEM
+async function loadTasks() {
+  const { data } = await supabase.from("tasks").select("*");
+
   const list = document.getElementById("taskList");
   list.innerHTML = "";
 
-  tasks.forEach((task, index) => {
-    list.innerHTML += `
-      <div class="task">
-        ${task}
-        <button class="delete" onclick="deleteTask(${index})">X</button>
-      </div>
-    `;
+  data.forEach(task => {
+    list.innerHTML += `<div class="task">${task.title}</div>`;
   });
-
-  localStorage.setItem("tasks", JSON.stringify(tasks));
 }
 
-function addTask() {
+async function addTask() {
   const input = document.getElementById("taskInput");
-  if (input.value.trim() === "") return;
 
-  tasks.push(input.value);
+  await supabase.from("tasks").insert([
+    { title: input.value }
+  ]);
+
   input.value = "";
-  renderTasks();
+  loadTasks();
 }
 
-function deleteTask(index) {
-  tasks.splice(index, 1);
-  renderTasks();
-}
-
-renderTasks();
+// AUTO CHECK LOGIN
+checkUser();
